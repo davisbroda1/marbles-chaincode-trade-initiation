53cc0327955e8774572294ccf7ae32d0364b33ba
Davis Broda <davisbroda@Daviss-MacBook-Pro.local>
2016-06-27 09:05:05 -0400

    Adding a print statement so I can see whether updated chaincode is being used.

--- part2_v1.0.0/part2_works_with_sdk_v1.0.0.go
+++ part2_v1.0.0/part2_works_with_sdk_v1.0.0.go
@@ -134,7 +134,7 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	fmt.Print("\n\n====================================================\n"+
 		"Checking to see if updated code being used"+
 		"\n====================================================\n\n")
-	
+
 	// Handle different functions
 	if function == "init" {													// initialize the chaincode state, used as reset
 		return t.init(stub, args)
